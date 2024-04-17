19eae526d1b94693c0e101d834c1aa8358d2a9c0
vinicius <vinc-analist@hotmail.com>
2024-04-16 22:31:22 -0300

    Initial commit

---
+++ src/main/java/com/example/challengebackendjava/ChallengeBackendJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.challengebackendjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeBackendJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeBackendJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/challengebackendjava/ChallengeBackendJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.challengebackendjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeBackendJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
